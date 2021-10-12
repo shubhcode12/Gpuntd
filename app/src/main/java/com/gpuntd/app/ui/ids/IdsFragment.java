80df30ed00946e09d1bd604033a40ddd05415e6d
shubhcode12 <shubhmahajan1234@gmail.com>
2021-10-12 22:59:55 +0530

    fragment_ids done

--- app/src/main/java/com/gpuntd/app/ui/ids/IdsFragment.java
+++ app/src/main/java/com/gpuntd/app/ui/ids/IdsFragment.java
@@ -5,12 +5,15 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.viewpager2.widget.ViewPager2;
 
+import com.google.android.material.tabs.TabLayout;
 import com.gpuntd.app.databinding.FragmentIdsBinding;
-import com.gpuntd.app.databinding.FragmentOffersBinding;
 
 
 public class IdsFragment extends Fragment {
@@ -24,6 +27,38 @@ public class IdsFragment extends Fragment {
         View root = binding.getRoot();
 
 
+        binding.tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                switch (tab.getPosition()) {
+
+                    case 1:
+                        binding.rvMyID.setVisibility(View.GONE);
+                        binding.rvCreateID.setVisibility(View.VISIBLE);
+                        break;
+
+                    default:
+                        binding.rvCreateID.setVisibility(View.GONE);
+                        binding.rvMyID.setVisibility(View.VISIBLE);
+                        break;
+
+                }
+
+
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+        binding.tabLayout.selectTab(binding.tabLayout.getTabAt(1));
         return root;
     }
 
