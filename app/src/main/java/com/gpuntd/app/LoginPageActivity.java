c5309d917b19fbaf342bda96541c78a1aa3677da
shubhcode12 <shubhmahajan1234@gmail.com>
2021-10-15 20:24:24 +0530

    loginpage done

--- app/src/main/java/com/gpuntd/app/LoginPageActivity.java
+++ app/src/main/java/com/gpuntd/app/LoginPageActivity.java
@@ -3,6 +3,7 @@ package com.gpuntd.app;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.gpuntd.app.databinding.ActivityLoginPageBinding;
@@ -15,5 +16,11 @@ public class LoginPageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_login_page);
+        binding.btnLogin.setOnClickListener(view -> {
+            startActivity(new Intent(LoginPageActivity.this, MainActivity.class));
+        });
+        binding.btnLoginWithOtp.setOnClickListener(view -> {
+            startActivity(new Intent(LoginPageActivity.this, OtpLoginActivity.class));
+        });
     }
 }
