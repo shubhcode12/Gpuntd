18e09a56bd6cc24d18bb68b1a5806aa87958061c
shubhcode12 <shubhmahajan1234@gmail.com>
2021-10-16 10:07:56 +0530

    navDrawer fixed

--- app/src/main/java/com/gpuntd/app/MainActivity.java
+++ app/src/main/java/com/gpuntd/app/MainActivity.java
@@ -4,10 +4,14 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Switch;
 import android.widget.Toast;
 
@@ -15,8 +19,11 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.cardview.widget.CardView;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -42,7 +49,7 @@ public class MainActivity extends AppCompatActivity {
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-        toolbar = (Toolbar) findViewById(R.id.toolbarMain);
+        toolbar = findViewById(R.id.toolbarMain);
         drawerLayout = findViewById(R.id.drawer_layout);
         navViewSide = findViewById(R.id.sideNav_view);
 
@@ -57,7 +64,7 @@ public class MainActivity extends AppCompatActivity {
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.navigation_home, R.id.navigation_offers, R.id.navigation_passbook, R.id.navigation_ids)
                 .build();
-         navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         // NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
 
@@ -73,10 +80,10 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        switch (item.getItemId()){
+        switch (item.getItemId()) {
             case R.id.action_withdrawDetails:
                 startActivity(new Intent(MainActivity.this, WithdrawDetailsActivity.class));
-               // navController.navigate(R.id.navigation_ids);
+                // navController.navigate(R.id.navigation_ids);
                 Toast.makeText(this, "" + item.getTitle(), Toast.LENGTH_SHORT).show();
                 break;
             case R.id.action_notification:
@@ -93,12 +100,12 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    private void setupNavigationDrawer(){
+    private void setupNavigationDrawer() {
         navViewSide.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @SuppressLint("WrongConstant")
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                switch (item.getItemId()){
+                switch (item.getItemId()) {
                     case R.id.drawer_profile:
                         drawerLayout.closeDrawer(Gravity.START, false);
                         startActivity(new Intent(MainActivity.this, ProfileActivity.class));
@@ -121,11 +128,12 @@ public class MainActivity extends AppCompatActivity {
                         break;
                     case R.id.drawer_help:
                         drawerLayout.closeDrawer(Gravity.START, false);
-                        startActivity(new Intent(MainActivity.this, ProfileActivity.class));
+                        startActivity(new Intent(MainActivity.this, HelpActivity.class));
                         break;
 
                     case R.id.drawer_logout:
-
+                        drawerLayout.closeDrawer(Gravity.START, false);
+                        setupLogoutDialog();
                         break;
 
                 }
@@ -139,6 +147,23 @@ public class MainActivity extends AppCompatActivity {
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
 
+    }
+
+    public void setupLogoutDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        View view = LayoutInflater.from(MainActivity.this).inflate(R.layout.logout_dialog, findViewById(R.id.logoutView));
+        builder.setView(view);
+        final AlertDialog alertDialog = builder.create();
+        view.findViewById(R.id.btnLogoutDialog).setOnClickListener(view1 -> {
+            Toast.makeText(MainActivity.this, "Logging out!", Toast.LENGTH_SHORT).show();
+        });
+        view.findViewById(R.id.btnCancelDialog).setOnClickListener(view1 -> {
+            alertDialog.dismiss();
+        });
+        if (alertDialog.getWindow() != null){
+            alertDialog.getWindow().setBackgroundDrawable(new ColorDrawable(0));
+        }
+        alertDialog.show();
     }
 
 
