a5a1705883795e796c5ceec28455ed9c28f73270
shubhcode12 <shubhmahajan1234@gmail.com>
2021-10-16 11:01:06 +0530

    help activity done

---
+++ app/src/main/java/com/gpuntd/app/HelpActivity.java
@@ -0,0 +1,46 @@
+package com.gpuntd.app;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Toast;
+
+import com.gpuntd.app.databinding.ActivityHelpBinding;
+
+public class HelpActivity extends AppCompatActivity {
+    ActivityHelpBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_help);
+
+        binding.wasapSection.setOnClickListener(view -> {
+            setupAddWhatsappDialog();
+        });
+    }
+
+    public void setupAddWhatsappDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        View view = LayoutInflater.from(HelpActivity.this).inflate(R.layout.add_whatsapp_dialog, findViewById(R.id.logoutView));
+        builder.setView(view);
+        final AlertDialog alertDialog = builder.create();
+        view.findViewById(R.id.addWhatsappEt);
+        view.findViewById(R.id.btnSubmitWp).setOnClickListener(view1 -> {
+            Toast.makeText(HelpActivity.this, "WhatsApp number submitted!", Toast.LENGTH_SHORT).show();
+        });
+        view.findViewById(R.id.btnCancelDialogWp).setOnClickListener(view1 -> {
+            alertDialog.dismiss();
+        });
+        if (alertDialog.getWindow() != null) {
+            alertDialog.getWindow().setBackgroundDrawable(new ColorDrawable(0));
+        }
+        alertDialog.show();
+    }
+
+}
